18fd5212f8779b69e49399ece9d57491ab097570
hyunchoitan <60224577+hyunchoitan@users.noreply.github.com>
2020-12-19 16:32:22 +0800

    Initialize game part.2 : Start timer when press play btn

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 'use strict';
 
 const gameField = document.querySelector(".game-field");
+const gameFieldSize = gameField.getBoundingClientRect()
 const playBtn = document.querySelector(".play-btn");
 const replayBtn = document.querySelector(".pop-up_replay-btn");
+const timer = document.querySelector(".timer");
 
 const NUM_OF_ITEMS = 9;
-const IMG_SIZE = 80;    
+const IMG_SIZE = 80;
+;
+
+let countdown
+let started = undefined;
+let count = 10
 
 
-// Game ststus
+// Game status
 
 
 const initGame = () => {
@@ -18,17 +25,17 @@ const initGame = () => {
 
 const startGame = () => {
     removeAllItems()
+    startTimer()
     initGame()
 }
 
 // placing Items in random position
 
 const getRandomNumber = (min, max) => {
-    return Math.random()*(max- min-80);
+    return Math.random()*(max- min- IMG_SIZE);
 }
 
 const getRandomPosition = () => {
-    const gameFieldSize = gameField.getBoundingClientRect()
     const randomX = Math.floor(getRandomNumber(gameFieldSize.left, gameFieldSize.right))
     const randomY = Math.floor(getRandomNumber(gameFieldSize.top, gameFieldSize.bottom))
     return [randomX, randomY]
@@ -58,9 +65,27 @@ const removeAllItems = () => {
 
 // Timer
 
+const paintText = () => {
+    if(count>=0){
+        const minutes = Math.floor(count/60);
+        const seconds = Math.floor(count%60);
+        const TimerMin = minutes <10 ? `0${minutes}`:minutes
+        const TimerSec = seconds <10 ? `0${seconds}`:seconds
+        return timer.innerText = `${TimerMin}:${TimerSec}`
+    }
+}
+
+const startTimer = () => {
+        setInterval(countdown=()=>{
+                paintText()
+                count--
+        },1000)
+}
+
+
+// Initialize
 
 const init = () => {
-    initGame();
     playBtn.addEventListener("click", startGame)
     replayBtn.addEventListener("click", startGame)
 
