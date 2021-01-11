b007cd28949b49b94ebe056ffeec072f52fcaa35
hyunchoitan <60224577+hyunchoitan@users.noreply.github.com>
2021-01-11 22:54:03 +0800

    Create Game Module with Game class

---
+++ js/game.js
@@ -0,0 +1,84 @@
+import PopUp from "./popUp.js"
+import Field from "./field.js"
+import Visibility from "./visibility.js"
+import Timer from "./timer.js"
+import * as sound from "./sound.js"
+
+
+export default class Game {
+    constructor(gameDuration, itemCounts){
+        this.gameDuration = gameDuration;
+        this.itemCounts = itemCounts;
+
+        this.playBtn = document.querySelector(".play-btn");
+        this.replayBtn = document.querySelector(".pop-up_replay-btn");
+        this.score = document.querySelector(".score")
+
+        this.started = false;
+
+        this.gameField = new Field(itemCounts)
+        this.finishBanner = new PopUp()
+        this.elements = new Visibility()
+        this.gameTimer = new Timer(gameDuration)
+        this.gameTimer.setStopGameListener((message)=>this.stop(message))
+
+        this.playBtn.addEventListener("click", ()=>{
+            if(this.started){
+                sound.playAlert()
+                this.stop("Try Again?")
+            } else if(!this.started) {
+                this.start()
+            }
+        })
+        this.replayBtn.addEventListener("click", this.replay)
+        this.gameField.setClickListener(this.clickItems)
+
+    }
+
+    start= () => {
+        this.gameTimer.start()
+        this.gameField.initField()
+        this.gameField.initGame()
+        this.elements.showStop()
+        this.elements.showTimerAndScore(this.itemCounts)
+        sound.playBg()
+        this.started = !this.started
+    }
+    
+    stop= (text) => {
+        this.finishBanner.showMessage(text)
+        this.gameTimer.stop()
+        this.gameField.initField()
+        this.finishBanner.toggle()
+        sound.pauseBg()
+        this.elements.hideStart()
+    }
+    
+    replay= () => {
+        this.finishBanner.toggle()
+        this.start()
+        this.elements.showStop()
+        this.started = !this.started
+    }
+
+    
+clickItems = (item) => {
+    if(item === "carrot") {
+        sound.playCarrot()
+        this.changeScore()
+    }
+    else if(item === "bug") {
+        sound.playBug()
+        this.stop("You Lost:(")
+    }
+}
+
+changeScore = () => {
+    const carrots = document.getElementsByClassName("carrot")
+    this.score.innerText = carrots.length
+    if(carrots.length === 0) {
+        sound.playWin()
+        this.stop("You Won:)")
+    }
+}
+}
--- js/main.js
+++ js/main.js
@@ -1,95 +1,10 @@
 'use strict';
 
-import PopUp from "./popUp.js"
-import Field from "./field.js"
-import Visibility from "./visibility.js"
-import Timer from "./timer.js"
-import * as sound from "./sound.js"
+import Game from "./game.js"
 
-const playBtn = document.querySelector(".play-btn");
-const replayBtn = document.querySelector(".pop-up_replay-btn");
-const score = document.querySelector(".score")
 
 const NUM_OF_ITEMS = 10;
 const GAME_DURATION_SEC = 10;
 
+const game = new Game(GAME_DURATION_SEC, NUM_OF_ITEMS)
 
-let started = false;
-
-
-const gameField = new Field(NUM_OF_ITEMS)
-const finishBanner = new PopUp()
-const elements = new Visibility()
-const gameTimer = new Timer(GAME_DURATION_SEC)
-gameTimer.setStopGameListener((message)=>stopGame(message))
-
-// Game status
-
-
-const startGame = () => {
-    gameTimer.start()
-    gameField.initField()
-    gameField.initGame()
-    elements.showStop()
-    elements.showTimerAndScore(NUM_OF_ITEMS)
-    sound.playBg()
-    started = !started
-}
-
-const stopGame = (text) => {
-    finishBanner.showMessage(text)
-    gameTimer.stop()
-    gameField.initField()
-    finishBanner.toggle()
-    sound.pauseBg()
-    elements.hideStart()
-}
-
-const replayGame = () => {
-    finishBanner.toggle()
-    startGame()
-    elements.showStop()
-    started = !started
-}
-
-
-// click Items
-
-const clickItems = (item) => {
-    if(item === "carrot") {
-        sound.playCarrot()
-        changeScore()
-    }
-    else if(item === "bug") {
-        sound.playBug()
-        stopGame("You Lost:(")
-    }
-}
-
-const changeScore = () => {
-    const carrots = document.getElementsByClassName("carrot")
-    score.innerText = carrots.length
-    if(carrots.length === 0) {
-        sound.playWin()
-        stopGame("You Won:)")
-    }
-}
-
-
-// Initialize
-
-const init = () => {
-    playBtn.addEventListener("click", ()=>{
-        if(started){
-            sound.playAlert()
-            stopGame("Try Again?")
-        } else if(!started) {
-            startGame()
-        }
-    })
-    replayBtn.addEventListener("click", replayGame)
-    gameField.setClickListener(clickItems)
-
-}
-
-init()
